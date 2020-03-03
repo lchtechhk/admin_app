34a8490110c33d14c4ee26403b27f941e722154a
DESKTOP-HQI4FAG\jamie <jamielamproject>
2020-03-03 18:25:47 +0800

    1. To do category tab

--- src/app/category-tab/category-tab.component.ts
+++ src/app/category-tab/category-tab.component.ts
@@ -8,6 +8,14 @@ import { Component} from '@angular/core';
 })
 export class CategoryTabComponent {
  
+  slideOpts = {
+    // spaceBetween:10,
+    // centeredSlides:true,
+    // slidersPerView:1.6,
+    initialSlide: 3,
+    speed: 400
+  };
+
   constructor(
  
   ) {console.log("CategoryTabComponent"); }
