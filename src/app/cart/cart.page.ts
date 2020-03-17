b450c59b80efe407b0667a560a4255dfa0838e7f
DESKTOP-HQI4FAG\jamie <jamielamproject>
2020-03-17 17:23:12 +0800

    1. to do cart

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigProvider } from '../providers/ConfigProvider';
+import { SharedDataProvider } from '../providers/shared-data';
 
 @Component({
   selector: 'app-cart',
@@ -7,12 +8,57 @@ import { ConfigProvider } from '../providers/ConfigProvider';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-
+  total: any;
   constructor(
-    public config : ConfigProvider
+    public config : ConfigProvider,
+    private sharedDataProvider: SharedDataProvider,
   ) { }
 
   ngOnInit() {
   }
+  
+  ionViewWillEnter() {
+    console.log("Cart is viewed");
+    this.totalPrice()
+  }
+
+  removeCart(id) {
+    this.sharedDataProvider.removeCart(id);
+    this.totalPrice();
+  }
+  qunatityPlus = function (q) {
+    q.customers_basket_quantity++;
+    q.subtotal = q.final_price * q.customers_basket_quantity;
+    q.total = q.subtotal;
+    if (q.customers_basket_quantity > q.quantity) {
+      q.customers_basket_quantity--;
+      this.shared.toast('Product Quantity is Limited!', 'short', 'center');
+    }
+    this.totalPrice();
+    this.shared.cartTotalItems();
+    this.storage.set('cartProducts', this.shared.cartProducts);
+  }
+  //function decreasing the quantity
+  qunatityMinus = function (q) {
+    if (q.customers_basket_quantity == 1) {
+      return 0;
+    }
+    q.customers_basket_quantity--;
+    q.subtotal = q.final_price * q.customers_basket_quantity;
+    q.total = q.subtotal;
+    this.totalPrice();
+
+    this.shared.cartTotalItems();
+    this.storage.set('cartProducts', this.shared.cartProducts);
+  }
+
+  totalPrice() {
+    var price = 0;
+    for (let value of this.sharedDataProvider.cartProducts) {
+      var pp = value.final_price * value.customers_basket_quantity;
+      price = price + pp;
+    }
+    this.total = price;
+  };
 
 }
--- src/app/providers/shared-data.ts
+++ src/app/providers/shared-data.ts
@@ -11,6 +11,8 @@ import { Platform } from '@ionic/angular';
 
 @Injectable()
 export class SharedDataProvider {
+    public cartProducts = new Array();
+
     constructor(
         // private fcm: FCM,
         public storage: Storage,
@@ -23,64 +25,81 @@ export class SharedDataProvider {
         private platform: Platform,
 
     ) {
-        // this.is_login();
     }
-    async is_login() {
-     
-    }
-    async getToken(){
+    async getToken() {
         try {
             let token = await this.storage.get('token');
             return token;
         }
-        catch(e) { console.log(e) }
+        catch (e) { console.log(e) }
     }
-    async setToken(token){
+    async setToken(token) {
         try {
             await this.removeToken();
             await this.storage.set('token', token);
         }
-        catch(e) { console.log(e) }
+        catch (e) { console.log(e) }
     }
-    async removeToken(){
+    async removeToken() {
         await this.storage.remove("token");
     }
 
-    async get_storage_key(key){
+    async get_storage_key(key) {
         try {
             let token = await this.storage.get(key);
             return token;
         }
-        catch(e) { console.log(e) }
+        catch (e) { console.log(e) }
     }
-    async set_storage_key(key,value){
+    async set_storage_key(key, value) {
         try {
             await this.remove_storage_key(key);
             await this.storage.set(key, value);
         }
-        catch(e) { console.log(e) }
+        catch (e) { console.log(e) }
     }
-    async remove_storage_key(key){
+    async remove_storage_key(key) {
         await this.storage.remove(key);
     }
- 
+
+    removeCart(p) {
+        this.cartProducts.forEach((value, index) => {
+            if (value.cart_id == p) {
+                this.cartProducts.splice(index, 1);
+                this.storage.set('cartProducts', this.cartProducts);
+            }
+        });
+        this.cartTotalItems();
+    }
+
+    cartTotalItems = function () {
+        this.events.publish('cartChange');
+        let total = 0;
+        for (let value of this.cartProducts) {
+          total += value.customers_basket_quantity;
+        }
+        this.cartquantity = total;
+        // console.log("updated");
+        return total;
+    };
+
     public update_fcm() {
-    //     if (this.platform.is("android") || this.platform.is("ios")) {
-    //         this.fcm.getToken().then(token => {
-    //             this.uiProvider.presentLoadingDefault();
-    //             this.config.post(this.config.url + 'update_fcm', { fcm: token, user_id: this.person_id }, (res: any) => {
-    //                 if (!this.ObjectUtils.isEmptyField(res) && !this.ObjectUtils.isEmptyField(res.data) && res.status) {
-    //                     this.router.navigateByUrl("/home/tab1", { replaceUrl: true });
-    //                 } else {
-    //                     alert(JSON.stringify(res.data))
-    //                 }
-    //                 this.uiProvider.dismissLoadingDefault();
-    //             }, (error: any) => {
-    //                 this.uiProvider.dismissLoadingDefault();
-    //                 alert(JSON.stringify(error))
-    //             });
-    //         });
-    //     }
+        //     if (this.platform.is("android") || this.platform.is("ios")) {
+        //         this.fcm.getToken().then(token => {
+        //             this.uiProvider.presentLoadingDefault();
+        //             this.config.post(this.config.url + 'update_fcm', { fcm: token, user_id: this.person_id }, (res: any) => {
+        //                 if (!this.ObjectUtils.isEmptyField(res) && !this.ObjectUtils.isEmptyField(res.data) && res.status) {
+        //                     this.router.navigateByUrl("/home/tab1", { replaceUrl: true });
+        //                 } else {
+        //                     alert(JSON.stringify(res.data))
+        //                 }
+        //                 this.uiProvider.dismissLoadingDefault();
+        //             }, (error: any) => {
+        //                 this.uiProvider.dismissLoadingDefault();
+        //                 alert(JSON.stringify(error))
+        //             });
+        //         });
+        //     }
         this.router.navigateByUrl("/home/tab1", { replaceUrl: true });
 
     }
